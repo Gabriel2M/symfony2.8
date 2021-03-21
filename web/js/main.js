e7fb04a49e010b4e0e4b08aa76f031f207b80eeb
Gabriel2M <gabriel.mendes.rf@gmail.com>
2021-03-21 13:56:37 -0300

    Add modal confirmation

---
+++ web/js/main.js
@@ -0,0 +1,22 @@
+// modal confirmation
+$('form[modal-confirmation]').submit(function (event) {
+    let $form = $(this),
+        $modal = $('#confirmationModal')
+
+    if ($modal.data('result') !== 'confirm') {
+        $modal.find(".confirmation-modal-message")
+            .text($form.attr('modal-confirmation'))
+
+        $modal.find('.btn.confirm').off('click').click(function () {
+            $modal.data('result', 'confirm')
+            $form.find('.btn[type="submit"]').attr('disabled', 'disabled')
+            $form.submit()
+        })
+
+        $modal.modal('show')
+
+        return false
+    }
+
+    return true
+})
